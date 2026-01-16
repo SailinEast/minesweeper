116356f065dd4adc964794862d3a1d3a7e2dd6ce
Sail <anasboranbaev@gmail.com>
2026-01-16 11:27:30 +0500

    Some changes for executable file

--- src/Game.java
+++ src/Game.java
@@ -14,8 +14,16 @@ public class Game {
     }
 
     public static void clearScreen() {
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
+        try {
+            if (System.getProperty("os.name").contains("Windows")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                System.out.println("\\033[H\\033[2J");
+                System.out.flush();
+            }
+        } catch (Exception e) {
+            for (int i = 0; i < 50; i++) System.out.println();
+        }
     }
 
     public void start() {
@@ -62,6 +70,9 @@ public class Game {
                 System.out.println("Error. Input correct numbers!");
             }
         }
+
+        System.out.println("Press 'enter' to exit...");
+        scanner.nextLine();
     }
 
     public static void main(String[] args) {
